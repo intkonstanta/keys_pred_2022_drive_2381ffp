a570b6b9a54495ac505a4bd4a7cdcc30d592ec16
intkonstanta <alexkonstantinov883@gmail.com>
2023-01-25 22:00:45 +0300

    1.2

--- rasp/local_client.py
+++ rasp/local_client.py
@@ -1,8 +1,11 @@
 import socket
+import gpio
+import time
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(("127.0.0.1", 8000))
 
 while True:
+
     letter = input()
     client.send(letter.encode("utf-8"))
--- server/local_server.py
+++ server/local_server.py
@@ -1,14 +1,21 @@
 import socket
+import asyncio
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # поднимаем сервак
-server.bind(("127.0.0.1", 8000))
+server.bind(("192.168.1.4", 8000))
 server.listen()
 
 while True:
     user, adrs = server.accept() # ждём подключение первого клиентов, и работаем с ним.
+    print(user, adrs)
     break # закрываем считывание новых клиентов
 
 while True:
+    letter = input()
+
+    user.send(letter.encode("utf-8"))
+
     data = user.recv(1024) # слушаем порт
+
     if data: # если чё-то есть, выводим
         print(data.decode("utf-8"))
