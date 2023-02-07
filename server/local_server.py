c75dc6384f2f7a8c163a41c5b02ef7e7f6751680
intkonstanta <alexkonstantinov883@gmail.com>
2023-02-07 03:36:18 +0300

    clear unused files and renaming

--- server/local_server.py
+++ server/local_server.py
@@ -1,25 +1,11 @@
 import socket
 
+UDP_IP = "127.0.0.1"
+UDP_PORT = 8000
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # поднимаем сервак
-server.bind(("192.168.1.4", 8000))
-all_users = []
-connect_flag = 0
-server.listen()
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
 
-while connect_flag == 0:
-    user, adress = server.accept()
-    if adress not in all_users:
-        all_users.append(adress)
-        print(all_users)
-        connect_flag = int(input())
-
-
-
- while True:
-     for user, adress in all_users:
-        data = user.recv(1024) # слушаем порт
-        if data: # если чё-то есть, выводим
-            data = data.decode("utf-8")
-
-
+while True:
+    data, addr = sock.recvfrom(1024)
+    print("received message: %s" % data)
