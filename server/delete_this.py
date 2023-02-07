e12e2509c8501b24bad41fc51d5f4134a4f67e3e
intkonstanta <alexkonstantinov883@gmail.com>
2023-02-07 03:06:45 +0300

    delete this

---
+++ server/delete_this.py
@@ -0,0 +1,17 @@
+import os
+import pathlib
+import json
+path_to_rasp = f"C:\\Users\\intkonstanta\\all_projects\\pred_drive\\rasp"
+base_dir = os.listdir(path_to_rasp)
+for file_name in base_dir:
+    path = pathlib.Path(path_to_rasp, file_name)
+    print(path)
+    try:
+        with open(path, "r") as file:
+            for line in file.readlines():
+                dict = json.loads(line)
+                print(dict["time"])
+    except:
+        pass
+
+
